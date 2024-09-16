3395ca6bc6267d821d7de958a864dadacd5937ca
rishang11 <rishang@coderixx.com>
2024-09-16 14:37:56 +0530

    add wallet and Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
 import Editor from "@/components/editor";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <Editor/>
+    <div className="flex items-center justify-center min-h-screen bg-slate-600">
+      <h1 className="text-center text-4xl text-white">Welcome to NovaTemp</h1>
+
+      {/* <Editor /> */}
     </div>
   );
 }
